dae7732f3dcc74ef4bf06e4a6b26f3803939335d
Gabriel <knuckles4x@gmail.com>
2020-08-20 20:35:41 -0300

    routes config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
-import GlobalStyles from "./styles/GlobalStyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import CustomersPanel from "./pages/CustomersPanel";
 
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <CustomersPanel />
+      <Routes>
+        <Route path="/" exact element={<CustomersPanel />} />
+        <Route path="/new_user" element={<h1>New user</h1>} />
+      </Routes>
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   );
 }
 
